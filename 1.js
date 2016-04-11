2dc3c1e51ed7272ee2a33eed97075d843ac414aa
krossio <krossio@gmail.com>
2016-04-11 09:40:26 -0600

    print odd 1 to 1000

---
+++ 1.js
@@ -0,0 +1,6 @@
+// Assignment: Print 1-255
+// Write a program that would print all the numbers from 1 to 255
+
+for (i = 1; i < 256; i++) {
+   console.log(i);
+}
---
+++ 2.js
@@ -0,0 +1,8 @@
+// Assignment: Odd Numbers
+// Write a program that would print all the odd numbers from 1 to 1000
+
+for (i = 0; i < 1001; 1++) {
+   if (i % 2 == 1) {
+      console.log(i);
+   }
+}
